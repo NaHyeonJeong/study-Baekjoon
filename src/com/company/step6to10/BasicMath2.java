1a656ee87268e8220aa16593e6b5b401cc552165
Hyeonjeong Na <pigismile@gmail.com>
2021-10-12 00:15:42 +0900

    2021.10.12 - 소수 공부

--- src/com/company/step6to10/BasicMath2.java
+++ src/com/company/step6to10/BasicMath2.java
@@ -2,6 +2,7 @@ package com.company.step6to10;
 
 import java.io.*;
 import java.math.BigInteger;
+import java.util.StringTokenizer;
 
 public class BasicMath2 {
     /**
@@ -68,4 +69,77 @@ public class BasicMath2 {
         bw.close();
     }
 
+    public void num1978() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        br.readLine(); //입력 받을 숫자 개수
+        int count = 0; //소수의 개수
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        while(st.hasMoreTokens()){
+            boolean isPrimeNum = true; //소수면 true, 아니면 false
+            int num = Integer.parseInt(st.nextToken());
+
+            if(num == 1){ //소수에서 1은 제외
+                continue;
+            }
+
+            //소수가 아닌 경우 - 나 자신이 아닌 값으로 나눠 떨어지기라도 하면 소수 탈락임
+            for(int i = 2; i <= Math.sqrt(num); i++){
+                if(num % i == 0){
+                    isPrimeNum = false;
+                    break;
+                }
+            }
+
+            //소수인 경우
+            if(isPrimeNum){
+                count++;
+            }
+        }
+
+        bw.write(count+"\n");
+        bw.flush();
+        bw.close();
+    }
+
+    public void num2581() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int M = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        int sum = 0;
+        int min = 100000;
+
+        for(int i = M; i <= N; i++){
+            boolean isPrimeNum = true; //소수면 true, 아니면 false
+
+            if(i == 1){ //1은 소수 아님
+                continue;
+            }
+
+            //소수가 아닌 경우 - 1이랑 나 자신이 아닌 값으로 나눠 떨어지기라도 하면 소수 탈락임
+            for(int j = 2; j <= Math.sqrt(i); j++){
+                if(i % j == 0){
+                    isPrimeNum = false;
+                    break;
+                }
+            }
+
+            if(isPrimeNum){ //소수인 경우
+                sum += i;
+                if(min > i){
+                    min = i;
+                }
+            }
+        }
+
+        if(sum == 0){
+            System.out.println(-1);
+        } else{
+            System.out.println(sum);
+            System.out.println(min);
+        }
+    }
 }
